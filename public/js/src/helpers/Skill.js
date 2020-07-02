3cab61fa9e192ba493c3350ed54c181e951cdd98
niknorine <nikitanorine@gmail.com>
2020-07-02 17:59:29 +0100

    Changed the skills to take in the JSON file for the skill variable

--- public/js/src/helpers/Skill.js
+++ public/js/src/helpers/Skill.js
@@ -1,7 +1,7 @@
 class Skill extends Phaser.GameObjects.Sprite{
-    constructor(scene,x,y, picture, name, character){
+    constructor(scene,x,y, picture, name, character, skill){
    
-        super(scene, x, y, picture, name, character)
+        super(scene, x, y, picture, name, character, skill)
         
         scene.sys.updateList.add(this);
         scene.sys.displayList.add(this);
@@ -13,29 +13,8 @@ class Skill extends Phaser.GameObjects.Sprite{
         }    
 
         this.skill = {
-            name: name,
-            character: character,
-            image: "mirage_step",
-            currentCoolDown: 0,
-            coolDownDuration: 2,
-            
-
-            cost: {
-                physical: 0,
-                magic: 0,
-                strategy: 0,
-                agility: 0,
-                unique: 0,
-                support: 1
-            },
-
-            effect:{
-                damage: 0,
-                damage_reduction: 100,
-                next_skill_reduction: "physical",
-                next_skill_reduction_ammount: 1,
-                lingering_damage: 0
-            }
+            skill
         }
+
     }
 }

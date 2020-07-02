bb9b8ab9fbbe32337fc251c352ee7f959304fdee
niknorine <nikitanorine@gmail.com>
2020-07-02 17:58:42 +0100

    added effects Class

---
+++ public/js/src/helpers/Effect.js
@@ -0,0 +1,33 @@
+class Effect extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y, picture, name, skill){
+   
+        super(scene, x, y, picture, name, skill)
+        
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+
+
+        this.info = {
+            name: name,
+            used: false
+            
+        }    
+
+        this.skill =         {
+            name: name,          
+            image: "mirage_step",
+            currentCoolDown: 0,
+            coolDownDuration: 2,
+            duration: 1,
+
+            effect:{
+                damage: 0,
+                damage_reduction: 100,
+                next_skill_reduction: "physical",
+                next_skill_reduction_ammount: 1,
+                lingering_damage: 0
+            }
+        }
+
+    }
+}
